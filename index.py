ff74f619565f18906e40ac4b45bd8fe34d8580c4
Laurent Raufaste <analogue@glop.org>
2011-04-03 15:00:49 -0400

    Added a method to detect if we are in production Use the 123 fb uid fake user on the development server

--- index.py
+++ index.py
@@ -14,62 +14,61 @@ from google.appengine.ext import db
 
 from Tapper import Tapper
 
+import tools
+
 
 def base64_url_decode(data):
 	data = data.encode(u'ascii')
 	data += '=' * (4 - (len(data) % 4))
 	return base64.urlsafe_b64decode(data)
 
+def get_fbuid_from_signed_request(signed_request):
+	app_secret = '3343b1814e225d22b55a92cbb04c970f'
+
+	try:
+		sig, payload = signed_request.split(u'.', 1)
+		sig = base64_url_decode(sig)
+		data = json.loads(base64_url_decode(payload))
+		
+		expected_sig = hmac.new(
+			self.app_secret, msg=payload, digestmod=hashlib.sha256).digest()
+
+		# allow the signed_request to function for upto 1 day
+		if sig == expected_sig and data[u'issued_at'] > (time.time() - 86400):
+			fb_user_id = data.get(u'user_id')
+	except ValueError, ex:
+		pass # ignore if can't split on dot
+	
+	return fb_user_id
+
 class MainHandler(webapp.RequestHandler):
 
-	app_secret = '3343b1814e225d22b55a92cbb04c970f'
 	app_id = '152524664810770'
+	test_uid = 123
 	passed = False
 	fb_user_id = None
 
 	def get(self):
-		# self.response.out.write('<html><body>GET You wrote:<pre>')
-		# self.response.out.write(cgi.escape(self.request.get('signed_request')))
-		# self.response.out.write('</pre></body></html>')
-		self.post()
+		if tools.on_dev_server():
+			self.display_index(self.test_uid)
 
 	def post(self):
 		signed_request = cgi.escape(self.request.get('signed_request'))
-		try:
-			sig, payload = signed_request.split(u'.', 1)
-			sig = base64_url_decode(sig)
-			data = json.loads(base64_url_decode(payload))
-			
-			expected_sig = hmac.new(
-				self.app_secret, msg=payload, digestmod=hashlib.sha256).digest()
-
-			# allow the signed_request to function for upto 1 day
-			if sig == expected_sig and data[u'issued_at'] > (time.time() - 86400):
-				self.passed = True
-				self.signed_request = data
-				self.fb_user_id = data.get(u'user_id')
-				self.access_token = data.get(u'oauth_token')
-		except ValueError, ex:
-			pass # ignore if can't split on dot
-		
-		if not self.fb_user_id:
+		fb_uid = get_fbuid_from_signed_request(signed_request)
+		self.display_index(fb_uid)
+	
+	def display_index(self, fb_uid):
+		if not fb_uid:
 			install_url = 'https://www.facebook.com/dialog/oauth?client_id='+self.app_id+'&redirect_uri=http://apps.facebook.com/tapotron/'
 			install_html = '<html><body><script type="text/javascript">parent.location.href="'+install_url+'";</script></body></html>'
 			self.response.out.write(install_html)
 		else:
-			query = db.GqlQuery("SELECT * FROM Tapper WHERE facebook_uid = :1", long(self.fb_user_id))
+			query = db.GqlQuery("SELECT * FROM Tapper WHERE facebook_uid = :1", long(fb_uid))
 			tapper = query.get()
 			if not tapper:
-				tapper = Tapper(facebook_uid=long(self.fb_user_id))
+				tapper = Tapper(facebook_uid = long(fb_uid))
 				self.response.out.write('New user!')
 			tapper.put()
 			template_values = {'uid': tapper.key()}
 			path = 'templates/index.html'
 			self.response.out.write(template.render(path, template_values))
-			self.response.out.write(signed_request)
-
-			# self.response.out.write('<html><body>POST You wrote:<pre>')
-			# self.response.out.write('Passed in if: '+str(self.passed))
-			# self.response.out.write(str(self.fb_user_id))
-			# self.response.out.write('</pre></body></html>')
-
---
+++ tools.py
@@ -0,0 +1,6 @@
+import os
+
+# Are we on the dev server?
+# Returns True if we are
+def on_dev_server():
+	return os.environ.get('SERVER_SOFTWARE', '').startswith('Development')
