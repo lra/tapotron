ed63191809cc5e4023730306f34af7ff08b47dfe
Thibaut <crenn@freshplanet.com>
2011-04-03 18:33:15 -0400

    Adds start button

--- cdn/script.js
+++ cdn/script.js
@@ -11,9 +11,9 @@ var timer;
 function sendResult() {
  $.get('/putScore/'+uid+'/'+count+'/', function(data) {
    if (data == 0) {
-     alert('error');
+     alert('Not your best score');
    } else {
-     alert('Score saved'); 
+     alert('High Score saved'); 
    }
  });
 }
@@ -40,13 +40,19 @@ function changeImage() {
 
 // registers keyboard and mouse to listen to each press
 function setUpListeners() {
-  $('body').click(function(event) {
-    incrementCounter(event);
-  });
+  $('body').click(
+    incrementCounter
+  );
 
-  $('body').keypress(function(event) {
-    incrementCounter(event);
-  });
+  $('body').keypress(
+    incrementCounter
+  );
+}
+
+function removeListeners() {
+  $('body').unbind('click',incrementCounter);
+  $('body').unbind('keypress',incrementCounter);
+
 }
 
 // set the first timestamp : start of the game
@@ -57,9 +63,9 @@ function startTimer(event) {
 
 function displayClicks() {
   var current = count - previous_count;
-  $('#graph-container').append('<div class="graph-points" style="height:'+(current*10+1)+'px">');
+  $('#graph-container').append('<div class="graph-points" style="height:'+(current + 1)+'px">');
   previous_count = count;
-  timer = setTimeout(displayClicks,100);
+  timer = setTimeout(displayClicks,1000);
 }
 
 
@@ -80,15 +86,35 @@ function incrementCounter(event) {
     } else {
       clearTimeout(timer);
       sendResult();
-      count = 0; 
+      stopGame();
+      count = 0;
+      previous_count = 0; 
     } 
   }
 }
 
+function stopGame() {
+  $('#start-button').show();
+  $('#button').hide();
+  removeListeners();
+}
+
+function startNewGame() {
+  $('#start-button').hide();
+  $('#button').fadeIn('slow', function() {});
+
+  $('#graph-container').empty();
+  changeImage();
+  setUpListeners(); 
+  $('#counter').text(count);
+  $('#time').text('0');
+
+
+}
+
 $(document).ready(function(){
   preloadImages(['button_up.png','button_down.png']);
-  changeImage();
-  setUpListeners();
+  $('#start-button').click(function(){ startNewGame(); });
 });
 
 
