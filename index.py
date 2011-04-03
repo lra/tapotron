51abcd0045f2d0a2111e1c4a1490ce35ef576f07
Laurent Raufaste <laurent@freshplanet.com>
2011-04-03 00:38:49 -0400

    Handler GET and POST

--- index.py
+++ index.py
@@ -1,5 +1,17 @@
+import cgi
+
 from google.appengine.ext import webapp
 
+
 class MainHandler(webapp.RequestHandler):
+
     def get(self):
-        self.response.out.write('Login Page')
+        self.response.out.write('<html><body>GET You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
+        
+
+    def post(self):
+        self.response.out.write('<html><body>POST You wrote:<pre>')
+        self.response.out.write(cgi.escape(self.request.get('content')))
+        self.response.out.write('</pre></body></html>')
